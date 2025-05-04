64fece1a93fd00e3645a6104baddc25eb23c402e
Johannes Roos <jhnnsrs@gmail.com>
2025-05-04 18:53:46 +0200

    fix: add missing imports and extend tests for reactify function

--- tests/test_reactify.py
+++ tests/test_reactify.py
@@ -1,9 +1,11 @@
 """Test the reactify function which converts a function or generator into an actor definition."""
 
-from typing import Generator
+from typing import Callable, Generator
 from rekuest_next.actors.actify import reactify
 from rekuest_next.api.schema import ActionKind
 from rekuest_next.structures.registry import StructureRegistry
+import pytest
+from .funcs import nested_basic_function, nested_structure_asyncgenerator, nested_structure_function
 
 
 def test_actify_function(simple_registry: StructureRegistry) -> None:
@@ -36,3 +38,17 @@ def test_actify_generator(simple_registry: StructureRegistry) -> None:
 
     defi, actor_builder = reactify(gen, simple_registry)
     assert defi.kind == ActionKind.GENERATOR
+
+
+@pytest.mark.parametrize(
+    "func",
+    [
+        nested_structure_asyncgenerator,
+        nested_structure_function,
+        nested_basic_function,
+    ],
+)
+def test_actify_matrix_functions(simple_registry: StructureRegistry, func: Callable) -> None:
+    """Test if different function types are correctly buildable into actor definitions."""
+
+    defi, actor_builder = reactify(func, simple_registry)
